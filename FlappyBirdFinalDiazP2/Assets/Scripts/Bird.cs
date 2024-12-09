5b4742e2498defafd40927832fd69a12022bb4eb
Joseph D <751728@student.dusd.net>
2024-12-09 10:20:28 -0800

    worked on project
    Added animations and started working on the animator

--- FlappyBirdFinalDiazP2/Assets/Scripts/Bird.cs
+++ FlappyBirdFinalDiazP2/Assets/Scripts/Bird.cs
@@ -2,11 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Bird : MonoBehaviour { 
+public class Bird : MonoBehaviour {
+    private float upForce = 200f;
 
     private bool isDead = false;
-    private Rigidbody2D rb2d;
+    private Rigidbody2D rb2d; 
     
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,8 +20,16 @@ public class Bird : MonoBehaviour {
     {
         if (isDead == false)
         {
-            rb2d.velocity = Vector2.zero;
-            rb2d.AddForce(new Vector2(0, upForce));
+            if (Input.GetMouseButtonDown (0))
+            {
+                rb2d.velocity = Vector2.zero;
+                rb2d.AddForce(new Vector2(0, upForce));
+            }
         }
+    }
+
+    void OnCollisionEnter2D ()
+    {
+        isDead = true;
     }
 }
